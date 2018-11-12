a956965bd77db57a722236d33fe2d948d14d8a66
virapArt <44981680+virapArt@users.noreply.github.com>
2018-11-12 21:35:01 +0200

    mygme

---
+++ eran/js/script.js
@@ -0,0 +1,277 @@
+function butone1() {
+		var img = document.getElementById('kov');
+		img.src = "img3/serojik1.jpg"
+	}
+
+function butone2() {
+		var img = document.getElementById('kov');
+		img.src = "img3/xach4.jpg"
+	}	
+
+function butone3() {
+		var img = document.getElementById('hov');
+		img.src = "img3/rub.jpg"
+	}
+
+function butone4() {
+		var img = document.getElementById('hov');
+		img.src = "img3/hovo.jpg"
+	}		
+function butone5() {
+		var img = document.getElementById('lov');
+		img.src = "img3/emul1.jpg"
+	}
+
+function butone6() {
+		var img = document.getElementById('lov');
+		img.src = "img3/vartov.jpg"
+	}	 
+function butone7() {
+		var img = document.getElementById('sov');
+		img.src = "img3/xchul.jpg"
+	}
+
+function butone8() {
+		var img = document.getElementById('sov');
+		img.src = "img3/zenq.jpg"
+	}	
+
+function butone9() {
+		var img = document.getElementById('mov');
+		img.src = "img3/eremul.jpg"
+	}
+
+function butone10() {
+		var img = document.getElementById('mov');
+		img.src = "img3/eremul2.jpg"
+	}					
+function butone11() {
+		var img = document.getElementById('ov');
+		img.src = "img3/menq.jpg"
+	}
+
+function butone12() {
+		var img = document.getElementById('ov');
+		img.src = "img3/rubo2.jpg"
+	}	
+function butone13() {
+		var img = document.getElementById('tov');
+		img.src = "img3/emms.jpg"
+	}
+
+function butone14() {
+		var img = document.getElementById('tov');
+		img.src = "img3/zenq2.jpg"
+	}	
+function butone15() {
+		var img = document.getElementById('xov');
+		img.src = "img3/ems.jpg"
+	}
+
+function butone16() {
+		var img = document.getElementById('xov');
+		img.src = "img3/emul2.jpg"
+	}		
+		
+function butone17() {
+		var img = document.getElementById('pov');
+		img.src = "img3/ems.jpg"
+	}
+
+function butone18() {
+		var img = document.getElementById('pov');
+		img.src = "img3/xch2.jpg"
+	}	
+function butone19() {
+		var img = document.getElementById('rov');
+		img.src = "img3/gox.jpg"
+	}
+
+function butone20() {
+		var img = document.getElementById('rov');
+		img.src = "img3/xhachul.jpg"
+	}			
+function butone21() {
+		var img = document.getElementById('zov');
+		img.src = "img3/lav.jpg"
+	}
+
+function butone22() {
+		var img = document.getElementById('zov');
+		img.src = "img3/tuyn.jpg"
+	}			
+
+function zxz() {
+    var snd1 = new Audio("mp3/apl2.mp3");
+  var cc=  snd1.play()
+}
+function apl3() {
+    var snd1 = new Audio("mp3/apl3.mp3");
+  var cc=  snd1.play()
+}
+function cap() {
+    var snd1 = new Audio("mp3/cap.mp3");
+  var cc=  snd1.play()
+}
+function smx() {
+    var snd1 = new Audio("mp3/smex.mp3");
+  var cc=  snd1.play()
+}
+function zxx() {
+    var snd1 = new Audio("mp3/sh3.mp3");
+  var cc=  snd1.play()
+}
+function png() {
+    var snd1 = new Audio("mp3/png.mp3");
+  var cc=  snd1.play()
+}
+function cng() {
+    var snd1 = new Audio("mp3/cng.mp3");
+  var cc=  snd1.play()
+}
+function lng() {
+    var snd1 = new Audio("mp3/lng.mp3");
+  var cc=  snd1.play()
+}
+function sig() {
+    var snd1 = new Audio("mp3/sig.mp3");
+  var cc=  snd1.play()
+}
+function xax() {
+    var snd1 = new Audio("mp3/xax.mp3");   
+  var cc=  snd1.play()
+}
+function sh2() {
+    var snd1 = new Audio("mp3/sh2.mp3");
+  var cc=  snd1.play()
+}
+
+function ini() {var plane = document.getElementById("bb",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function inii() {var plane = document.getElementById("bbb",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function iniii() {var plane = document.getElementById("bbbb",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function inter() {var dd = document.getElementById("bt",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }   
+
+function a() {var plane = document.getElementById("aa",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function aa() {var plane = document.getElementById("aaa",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function aaa() {var plane = document.getElementById("aaaa",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ab() {var dd = document.getElementById("a",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }           
+
+function c() {var plane = document.getElementById("c",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function cc() {var plane = document.getElementById("cc",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ccc() {var plane = document.getElementById("ccc",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function cccc() {var dd = document.getElementById("cccc",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }   
+
+function d() {var plane = document.getElementById("d",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function dd() {var plane = document.getElementById("dd",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ddd() {var plane = document.getElementById("ddd",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function dddd() {var dd = document.getElementById("dddd",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }  
+
+function e() {var plane = document.getElementById("e",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ee() {var plane = document.getElementById("ee",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function eee() {var plane = document.getElementById("eee",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function eeee() {var dd = document.getElementById("eeee",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }      
+
+function f() {var plane = document.getElementById("f",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ff() {var plane = document.getElementById("ff",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function fff() {var plane = document.getElementById("fff",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ffff() {var dd = document.getElementById("ffff",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    } 
+
+function r() {var plane = document.getElementById("r",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function rr() {var plane = document.getElementById("rr",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function rrr() {var plane = document.getElementById("rrr",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function rrrr() {var dd = document.getElementById("rrrr",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }       
+
+function l() {var plane = document.getElementById("l",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function ll() {var plane = document.getElementById("ll",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function lll() {var plane = document.getElementById("lll",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function llll() {var dd = document.getElementById("llll",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }   
+
+function m() {var plane = document.getElementById("m",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function mm() {var plane = document.getElementById("mm",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function mmm() {var plane = document.getElementById("mmm",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function mmmm() {var dd = document.getElementById("mmmm",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    } 
+
+function q() {var plane = document.getElementById("q",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function qq() {var plane = document.getElementById("qq",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function qqq() {var plane = document.getElementById("qqq",);
+        plane.innerHTML = "WRONG";        
+        plane.setAttribute("class", "xy");    }   
+function qqqq() {var dd = document.getElementById("qqqq",);
+        dd.innerHTML = "SUPER";        
+        dd.setAttribute("class", "yy");    }                                                                              
